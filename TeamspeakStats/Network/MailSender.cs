cb976ba37aafca79e9e41616726e5c7ab0589883
Coruscant11 <coruscant11@gmail.com>
2016-03-08 00:56:07 +0100

    Classe d'envois de mail

--- TeamspeakStats/Network/MailSender.cs
+++ TeamspeakStats/Network/MailSender.cs
@@ -5,14 +5,25 @@ using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
-
+using Outlook = Microsoft.Office.Interop.Outlook;
 namespace TeamspeakStats
 {
     class MailSender
     {
         public static void SendMail(string from, string destination, string text)
         {
-            
+            using (SmtpClient smtpClient = new SmtpClient("smtp.live.com"))
+            {
+                MailMessage message = new MailMessage();
+
+                message.From = new MailAddress("mabiteosblc@hotmail.com", "Coruscant11");
+                message.To.Add("mateox06@hotmail.fr");
+                message.Subject = "Subject email";
+                message.Body = "Content email";
+                message.IsBodyHtml = false;
+
+                smtpClient.SendAsync(message, null);
+            }
         }
     }
 }
